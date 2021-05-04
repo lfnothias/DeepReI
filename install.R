164bbd6105a1097c6934ce1c968c7b9c80a99f41
lfnothias <louisfelix.nothias@gmail.com>
2021-05-04 16:31:27 -0700

    Update install.R

--- install.R
+++ install.R
@@ -4,19 +4,3 @@ install.packages("httr")
 install.packages("shinydashboard")
 install.packages('leaflet')
 install.packages(“keras”)
-library(keras)
-install_keras()
-
-if (!requireNamespace("BiocManager", quietly = TRUE))
-    install.packages("BiocManager")
-
-BiocManager::install("ChemmineR")
-
-if (!requireNamespace("BiocManager", quietly = TRUE))
-    install.packages("BiocManager")
-
-BiocManager::install("ChemmineOB")
-
-devtools::install_github("TomasVrzal/DeepReI")
-
-tensorflow::install_tensorflow(version = “2.0.0")
